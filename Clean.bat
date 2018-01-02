63baaee0620cbe78cdb22c67c216082b3bad9b3f
Huang Rui <vowstar@gmail.com>
2018-01-02 16:17:23 +0800

    Add clean script

---
+++ Clean.bat
@@ -0,0 +1,11 @@
+del *.SchDocPreview
+del *.HarnessPreview
+del *.PcbDocPreview
+del *.TopPreview
+del *.PrjPcbStructure
+del *.PcbDoc.htm
+del *.PcbLib.htm
+del *.rep
+del *.csv
+rd History /s/q
+rd __Previews /s/q
